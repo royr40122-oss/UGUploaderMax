ef130bb1b10fe2c138bfe8007fdc802de7b3ae1b
royr40122-oss <royr40122@gmail.com>
2026-02-26 13:02:30 +0530

    Update API credentials and owner ID in vars.py

--- vars.py
+++ vars.py
@@ -2,9 +2,9 @@ import os
 from os import environ
 
 # API Configuration
-API_ID = int(os.environ.get("API_ID", "29061977"))
-API_HASH = os.environ.get("API_HASH", "9519882b3dcb5dd995ca75247bd934ea")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8420352907:AAHE7dOszs4w8sRHIHqIgzFUmmZya1HTMyM")
+API_ID = int(os.environ.get("API_ID", "31445635"))
+API_HASH = os.environ.get("API_HASH", "15c057ca3aa85360ac3d63de52368718")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8776759880:AAGBsD4g8-DRLBmgSSwtCWk0KI3OiXkxauQ")
 
 CREDIT = os.environ.get("CREDIT", "Jai shree Krishna ✨")
 # MongoDB Configuration
@@ -13,8 +13,8 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://anil07gurjar07:anil
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", "5371688792"))
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "5371688792").split()]  # Default to owner ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "8441048952"))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "8441048952").split()]  # Default to owner ID
 
 # Channel Configuration
 PREMIUM_CHANNEL = "https://t.me/+W-Q51EuLf2QwYTl"
@@ -59,6 +59,7 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
